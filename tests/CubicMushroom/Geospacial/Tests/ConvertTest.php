495fc272aab301235fd2e26106fa98d7694ad8d6
Toby Griffiths <toby@cubicmushroom.co.uk>
2012-12-09 14:02:56 +0000

    Added test for Convert::ENToLatLong() method using 6 test locations (UK postcodes)

--- tests/CubicMushroom/Geospacial/Tests/ConvertTest.php
+++ tests/CubicMushroom/Geospacial/Tests/ConvertTest.php
@@ -10,15 +10,90 @@
 
 namespace CubicMushroom\Geospacial;
 
+use CubicMushroom\Geospacial\GeoPoints;
+
+// var_export(realpath(__DIR__ . "/../../../../")); exit;
+
 require __DIR__ . "/../../../../vendor/autoload.php";
 
 class ConvertTest extends \PHPUnit_Framework_TestCase
 {
-    public function testE_N_to_Lat_Long()
+    /**
+     * Test conversion from Easting/Northing to Lat/Lon
+     */
+    public function testENToLatLong()
     {
-        $this->assertEqual(
-            array(),
-            Convert::E_N_to_Lat_Long();
+        // Postcodes to test
+        $points = array(
+            // LE67 2GN
+            array(
+                'EN'     => new GeoPoints\EastingNorthingGeoPoint(
+                    '442124', '313080'
+                ),
+                'LatLgn' => new GeoPoints\LatitudeLongitudeGeoPoint(
+                    52.71376507, -1.37787778
+                ),
+            ),
+            // BD17 5BS
+            array(
+                'EN'     => new GeoPoints\EastingNorthingGeoPoint(
+                    '414611', '439168'
+                ),
+                'LatLgn' => new GeoPoints\LatitudeLongitudeGeoPoint(
+                    53.84856175, -1.77939768
+                ),
+            ),
+            // TS5 6QD
+            array(
+                'EN'     => new GeoPoints\EastingNorthingGeoPoint(
+                    '449570', '518367'
+                ),
+                'LatLgn' => new GeoPoints\LatitudeLongitudeGeoPoint(
+                    54.55813452, -1.23503119
+                ),
+            ),
+            // ST1 2AE
+            array(
+                'EN'     => new GeoPoints\EastingNorthingGeoPoint(
+                    '388977', '347659'
+                ),
+                'LatLgn' => new GeoPoints\LatitudeLongitudeGeoPoint(
+                    53.02612209, -2.16579512
+                ),
+            ),
+            // RH6 0AN
+            array(
+                'EN'     => new GeoPoints\EastingNorthingGeoPoint(
+                    '526928', '142262'
+                ),
+                'LatLgn' => new GeoPoints\LatitudeLongitudeGeoPoint(
+                    51.16552667, -0.18599962
+                ),
+            ),
+            // NN18 9NE
+            array(
+                'EN'     => new GeoPoints\EastingNorthingGeoPoint(
+                    '487900', '287700'
+                ),
+                'LatLgn' => new GeoPoints\LatitudeLongitudeGeoPoint(
+                    52.48017216, -0.70713366
+                ),
+            )
         );
+        foreach ($points as $pointSet) {
+            $llPoint = Convert::ENToLatLong($pointSet['EN']);
+            $this->assertEquals(
+                $pointSet['LatLgn']->latitude,
+                $llPoint->latitude,
+                "Converted point's latitude value is not correct",
+                0.002
+            );
+            $this->assertEquals(
+                $pointSet['LatLgn']->longitude,
+                $llPoint->longitude,
+                "Converted point's longitude value is not correct",
+                0.002
+            );
+        }
     }
 }
